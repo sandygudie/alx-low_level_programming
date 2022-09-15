324cdb440c6bc77c8c5d507b44c1aeec25f8d39f
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2022-09-15 04:55:30 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - sum of two integers
+ *
+ * @a: integer number
+ * @b: integer number
+ *
+ * Return: sum of the numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
