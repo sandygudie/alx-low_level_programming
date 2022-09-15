ae46fa358452336354bea6bd660d117868ec2787
Goodnews Sandy <54219127+sandygudie@users.noreply.github.com>
2022-09-15 04:38:29 +0100

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - returns absolute value of n
+ * @n: accepts integer value
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
